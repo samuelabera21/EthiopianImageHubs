5642123361fb959b1ea8b0161bdbdf8f3eb21602
Samuel Abera <sami21.good.bad@gmail.com>
2026-07-27 16:02:53 +0300

    Image routes how to image get or image endpoints

---
+++ backend/src/routes/image.routes.ts
@@ -0,0 +1,59 @@
+import { Router } from "express";
+
+import { imageController } from "../controllers/image.controller";
+
+import { authenticate } from "../middlewares/auth.middleware";
+
+import { uploadSingleImage } from "../middlewares/upload.middleware";
+
+import { validate } from "../middlewares/validation.middleware";
+
+import { updateImageSchema, uploadImageSchema } from "../validators/image.validator";
+
+const router = Router();
+
+/**
+ * GET /images
+ */
+router.get(
+  "/",
+  imageController.getImages.bind(
+    imageController,
+  ),
+);
+
+/**
+ * GET /images/:imageId
+ */
+router.get(
+  "/:imageId",
+  imageController.getImageById.bind(
+    imageController,
+  ),
+);
+
+/**
+ * POST /images
+ */
+router.post(
+  "/",
+  authenticate,
+  uploadSingleImage,
+  validate(uploadImageSchema),
+  imageController.uploadImage.bind(imageController),
+);
+
+router.patch(
+  "/:imageId",
+  authenticate,
+  validate(updateImageSchema),
+  imageController.updateImage.bind(imageController),
+);
+
+router.delete(
+  "/:imageId",
+  authenticate,
+  imageController.deleteImage.bind(imageController),
+);
+
+export default router;
