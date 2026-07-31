b38501aeb769ac62a1bf7a797b80cd70a31615a8
Samuel Abera <sami21.good.bad@gmail.com>
2026-07-31 21:21:34 +0300

    update: client.ts

--- backend/src/generated/prisma/client.ts
+++ backend/src/generated/prisma/client.ts
@@ -12,14 +12,16 @@
 
 import * as process from 'node:process'
 import * as path from 'node:path'
+import { fileURLToPath } from 'node:url'
+globalThis['__dirname'] = path.dirname(fileURLToPath(import.meta.url))
 
 import * as runtime from "@prisma/client/runtime/client"
-import * as $Enums from "./enums"
-import * as $Class from "./internal/class"
-import * as Prisma from "./internal/prismaNamespace"
+import * as $Enums from "./enums.js"
+import * as $Class from "./internal/class.js"
+import * as Prisma from "./internal/prismaNamespace.js"
 
-export * as $Enums from './enums'
-export * from "./enums"
+export * as $Enums from './enums.js'
+export * from "./enums.js"
 /**
  * ## Prisma Client
  * 
@@ -49,6 +51,11 @@ export type Role = Prisma.RoleModel
  * 
  */
 export type User = Prisma.UserModel
+/**
+ * Model ContributorApplication
+ * 
+ */
+export type ContributorApplication = Prisma.ContributorApplicationModel
 /**
  * Model UserSession
  * 
