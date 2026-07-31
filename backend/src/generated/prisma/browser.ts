ff19cb01eb41ebd1b00daa9763a3bdc7d4fec7a7
Samuel Abera <sami21.good.bad@gmail.com>
2026-07-31 21:21:34 +0300

    update: browser.ts

--- backend/src/generated/prisma/browser.ts
+++ backend/src/generated/prisma/browser.ts
@@ -13,10 +13,10 @@
  * 🟢 You can import this file directly.
  */
 
-import * as Prisma from './internal/prismaNamespaceBrowser'
+import * as Prisma from './internal/prismaNamespaceBrowser.js'
 export { Prisma }
-export * as $Enums from './enums'
-export * from './enums';
+export * as $Enums from './enums.js'
+export * from './enums.js';
 /**
  * Model Role
  * 
@@ -27,6 +27,11 @@ export type Role = Prisma.RoleModel
  * 
  */
 export type User = Prisma.UserModel
+/**
+ * Model ContributorApplication
+ * 
+ */
+export type ContributorApplication = Prisma.ContributorApplicationModel
 /**
  * Model UserSession
  * 
