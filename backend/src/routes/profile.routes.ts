0a2ff4c25af335369d7e068866212063bd61e02c
Samuel Abera <sami21.good.bad@gmail.com>
2026-08-06 21:53:12 +0300

    chore: update profile.routes.ts

--- backend/src/routes/profile.routes.ts
+++ backend/src/routes/profile.routes.ts
@@ -1,7 +1,31 @@
 import { Router } from "express";
 import { profileController } from "../controllers/profile.controller";
+import { authenticate } from "../middlewares/auth.middleware";
+import { authorize } from "../middlewares/authorize.middleware";
+import { uploadSingleImage } from "../middlewares/upload.middleware";
 
 const router = Router();
+
+/**
+ * PATCH /profiles/me
+ */
+router.patch(
+  "/me",
+  authenticate,
+  authorize(["USER", "CONTRIBUTOR", "ADMIN"]),
+  profileController.updateProfile.bind(profileController)
+);
+
+/**
+ * PATCH /profiles/me/avatar
+ */
+router.patch(
+  "/me/avatar",
+  authenticate,
+  authorize(["USER", "CONTRIBUTOR", "ADMIN"]),
+  uploadSingleImage,
+  profileController.updateAvatar.bind(profileController)
+);
 
 /**
  * GET /profiles/:username
