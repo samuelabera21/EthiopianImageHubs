a16abafe351f0d953f710208d70c97458299f207
Samuel Abera <sami21.good.bad@gmail.com>
2026-08-06 21:53:11 +0300

    chore: update category.routes.ts

--- backend/src/routes/category.routes.ts
+++ backend/src/routes/category.routes.ts
@@ -1,17 +1,43 @@
 import { Router } from "express";
-
 import { categoryController } from "../controllers/category.controller";
+import { authenticate } from "../middlewares/auth.middleware";
+import { authorize } from "../middlewares/authorize.middleware";
 
 const router = Router();
 
 /**
  * GET /categories
  */
-router.get(
+router.get("/", categoryController.getCategories.bind(categoryController));
+
+/**
+ * POST /categories
+ */
+router.post(
   "/",
-  categoryController.getCategories.bind(
-    categoryController,
-  ),
+  authenticate,
+  authorize(["ADMIN"]),
+  categoryController.createCategory.bind(categoryController)
+);
+
+/**
+ * PATCH /categories/:id
+ */
+router.patch(
+  "/:id",
+  authenticate,
+  authorize(["ADMIN"]),
+  categoryController.updateCategory.bind(categoryController)
+);
+
+/**
+ * DELETE /categories/:id
+ */
+router.delete(
+  "/:id",
+  authenticate,
+  authorize(["ADMIN"]),
+  categoryController.deleteCategory.bind(categoryController)
 );
 
 export default router;
