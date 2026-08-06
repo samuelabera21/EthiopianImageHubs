5bdee585b112def5e67d2048557f4bb8efdb4fe5
Samuel Abera <sami21.good.bad@gmail.com>
2026-08-06 21:53:09 +0300

    chore: update category.controller.ts

--- backend/src/controllers/category.controller.ts
+++ backend/src/controllers/category.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 
 import { categoryRepository } from "../repositories/category.repository";
+import { categoryService } from "../services/category.service";
+import { categoryParamsSchema, createCategorySchema, updateCategorySchema } from "../validators/category.validator";
 
 export class CategoryController {
   /**
@@ -11,6 +13,37 @@ export class CategoryController {
       const categories = await categoryRepository.findAll();
 
       return res.json({ success: true, data: categories });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async createCategory(req: Request, res: Response, next: NextFunction) {
+    try {
+      const data = createCategorySchema.parse(req.body);
+      const result = await categoryService.createCategory(data);
+      return res.status(201).json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async updateCategory(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = categoryParamsSchema.parse(req.params);
+      const data = updateCategorySchema.parse(req.body);
+      const result = await categoryService.updateCategory(id, data);
+      return res.json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async deleteCategory(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = categoryParamsSchema.parse(req.params);
+      const result = await categoryService.deleteCategory(id);
+      return res.json(result);
     } catch (error) {
       next(error);
     }
